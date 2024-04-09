9b2c6c751e185045c6c95edc061e25ff1fb97e62
Samandar <samandarwebdeveloper@gmail.com>
2024-04-09 17:38:40 +0500

    add ctf-website

---
+++ pages/auth.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="w-full h-screen bg-white dark:bg-gray-800"></div>
+</template>
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({layout: 'default'})
+useHead({title: 'Home page'})
+</script>
+<template>
+    <section>
+        <h2>Home page</h2>
+         <NuxtLink to="/auth">Auth page</NuxtLink>
+    </section>
+</template>
