932a06f2bf06c7163facb540ca220178b1137254
Samandar <samandarwebdeveloper@gmail.com>
2024-04-28 13:28:25 +0500

    fix add table

---
+++ components/shared/table.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+    <table class="w-full text-sm text-left rtl:text-right text-white">
+      <thead class="text-xs text-white uppercase bg-darkColor">
+        <tr>
+          <th
+            v-for="(item, i) in heading"
+            :key="i"
+            scope="col"
+            class="px-6 py-3"
+          >
+            {{ item }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, i) in table"
+          :key="i"
+          class="bg-white border-b last:border-b-0 text-xl cursor-pointer text-darkColor hover:text-white hover:bg-metal"
+        >
+          <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+            {{ i + 1 }}
+          </th>
+          <td class="px-6 py-4">
+            <img src="../../assets/images/user-image.jpg" alt="user image" class="w-[60px] h-[60px] object-cover rounded-full"/>
+          </td>
+          <td class="px-6 py-4">
+            {{ item.username }}
+          </td>
+          <td class="px-6 py-4">
+            {{ item.country }}
+          </td>
+
+          <td class="px-6 py-4">
+            {{ item.date }}
+          </td>
+
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  heading: {
+    type: Array,
+    required: true,
+  },
+  table: {
+    type: Array,
+    required: true,
+  },
+});
+
+// const column = computed(() => {
+//   const arr = [];
+//   props.table.forEach((item: any) => {
+//     const result = Object.entries(item);
+//     const [key, value] = result;
+//   });
+// });
+
+// const imgFucn = (e:any) => {
+//     return require(`../../assets/images/${e}.jpg`)
+// }
+onMounted(() => {
+  console.log(column.value);
+});
+</script>
--- pages/challenge/[id].vue
+++ pages/challenge/[id].vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="pb-5">
     <h2 class="text-4xl font-bold text-darkColor text-center mt-[3%] mb-4">
       JavaScript
     </h2>
 
-    <div class="flex items-center justify-center gap-x-2 pb-6 border-b-2 border-darkColor mb-8">
+    <div
+      class="flex flex-col-reverse lg:flex-row items-center justify-center gap-y-2 gap-x-2 pb-6 border-b-2 border-darkColor mb-8"
+    >
       <div
         class="bg-blue text-white px-2 py-0.5 flex items-center rounded-md font-semibold"
       >
@@ -45,19 +47,147 @@
     </div>
 
     <div class="bg-white w-full rounded-lg shadow-lg px-3 py-4">
-       <div class="flex gap-x-2 mb-6">
-        <div class="bg-darkColor px-2 py-1 text-sm font-semibold uppercase text-white rounded-lg">
-            Kamikatze
+      <div class="flex gap-x-2 mb-6">
+        <div
+          class="bg-darkColor px-2 py-1 text-sm font-semibold uppercase text-white rounded-lg"
+        >
+          Kamikatze
         </div>
-        <div class="bg-tahiti px-2 py-1 text-sm font-semibold uppercase text-white rounded-lg">
-         April 25, 2024
+        <div
+          class="bg-tahiti px-2 py-1 text-sm font-semibold uppercase text-white rounded-lg"
+        >
+          April 25, 2024
         </div>
-       </div>
+      </div>
 
-       <div>
-        <h2 class="text-2xl mb-4 pb-3 border-dotted border-b-4 border-darkColor text-darkColor font-medium">Challenge desctiption</h2>
-        <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fugit consequatur sapiente alias earum fuga voluptate aperiam, debitis beatae adipisci voluptas assumenda molestiae. Deserunt facilis odit asperiores pariatur labore, numquam aut voluptatum consectetur! Sint et labore harum recusandae esse temporibus blanditiis dignissimos? Alias temporibus enim tempore dicta quae vitae illum adipisci soluta vero minus id eaque repellendus fugit delectus, accusantium magni natus? Ab similique excepturi dignissimos vel dicta labore aliquam ducimus iure. Quam cum amet molestiae similique sunt sapiente eaque at sit magni harum dolores quas, cumque, nobis suscipit. Ducimus adipisci expedita vitae deserunt quisquam earum sequi totam molestiae, qui accusamus inventore repellat officiis eius explicabo eaque quasi ad labore voluptatem facilis illum quia vero odio iste! Nulla deleniti modi mollitia nihil suscipit voluptatem quas velit nam incidunt, tenetur illo odit vel aspernatur et quisquam alias ipsa illum! Incidunt ipsa excepturi unde qui corrupti cupiditate omnis perspiciatis corporis beatae quia.</p>
-       </div>
+      <div>
+        <h2
+          class="text-2xl mb-4 pb-3 border-dotted border-b-4 border-darkColor text-darkColor font-medium"
+        >
+          Challenge desctiption
+        </h2>
+        <p class="text-justify mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          fugit consequatur sapiente alias earum fuga voluptate aperiam, debitis
+          beatae adipisci voluptas assumenda molestiae. Deserunt facilis odit
+          asperiores pariatur labore, numquam aut voluptatum consectetur! Sint
+          et labore harum recusandae esse temporibus blanditiis dignissimos?
+          Alias temporibus enim tempore dicta quae vitae illum adipisci soluta
+          vero minus id eaque repellendus fugit delectus, accusantium magni
+          natus? Ab similique excepturi dignissimos vel dicta labore aliquam
+          ducimus iure. Quam cum amet molestiae similique sunt sapiente eaque at
+          sit magni harum dolores quas, cumque, nobis suscipit. Ducimus adipisci
+          expedita vitae deserunt quisquam earum sequi totam molestiae, qui
+          accusamus inventore repellat officiis eius explicabo eaque quasi ad
+          labore voluptatem facilis illum quia vero odio iste! Nulla deleniti
+          modi mollitia nihil suscipit voluptatem quas velit nam incidunt,
+          tenetur illo odit vel aspernatur et quisquam alias ipsa illum!
+          Incidunt ipsa excepturi unde qui corrupti cupiditate omnis
+          perspiciatis corporis beatae quia.
+        </p>
+
+        <h3 class="text-xl text-darkColor mb-3">
+          Encrypted Flag: CBRPC.Z T:Fa{a0097105d36c58efYESAEU:CF
+          lg6acbb696e87ec814}
+        </h3>
+        <a
+          href=""
+          download="download challenge"
+          class="bg-blue p-2 inline-block text-white font-medium rounded-lg mb-2"
+        >
+          <IconCSS name="ph:download-light" class="text-white text-xl" />
+          Downoad</a
+        >
+
+        <div>
+          <input
+            type="text"
+            class="w-[500px] h-[38px] px-3 mr-4 text-base outline-none border border-darkColor text-darkColor rounded-xl"
+            placeholder="Flag{fasd577asd545asd7f}"
+          />
+          <button class="bg-blue px-6 py-1 text-white text-xl rounded-lg">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
+
+    <!-- table -->
+    <SharedTable :heading="heading" :table="table"/>
   </div>
 </template>
+<script setup lang="ts">
+const heading = ["#", "Image", "Username", "Country", "Date time"];
+const table = [
+  {
+    id: '1',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  { 
+    id: '2',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '3',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '4',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id:'5',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '6',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '7',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '8',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id:'9',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+  {
+    id: '10',
+    img: "user-image",
+    username: "Samandar",
+    country: "Uzbekistan",
+    date: "August 27, 2023 12:30 UTC",
+  },
+];
+</script>
