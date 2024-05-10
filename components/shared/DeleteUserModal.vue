8b3e8fde13e87ca2868c541e0b5a3b0145268f96
Samandar <samandarwebdeveloper@gmail.com>
2024-05-10 16:49:02 +0500

    fix user page

--- components/shared/DeleteUserModal.vue
+++ components/shared/DeleteUserModal.vue
@@ -5,10 +5,14 @@
     :class="`${modalVisible ? 'visible duration-75' : 'invisible'}`"
   ></div>
   <div
-    class="fixed top-[30%] left-[13%] md:top-1/3 md:left-1/3 w-[300px] lg:w-1/4 h-auto z-40 bg-white rounded-lg py-4 px-4"
+    class="fixed top-[30%] left-[13%] md:top-1/3 md:left-1/3 w-[300px] lg:w-1/4 h-auto z-40 bg-darkColor text-white rounded-lg py-4 px-4"
     :class="`${modalVisible ? 'visible' : 'invisible'}`"
   >
-  
+  <h2 class="text-center text-xl font-medium mb-2">Delete the Samandar</h2>
+  <div class="flex items-center justify-evenly">
+    <button class="bg-red px-3 py-1 rounded-md text-lg font-medium">Delete</button>
+    <button @click="$emit('closeModal')" class="bg-white text-blakc px-3 py-1 rounded-md text-lg font-medium">Cancel</button>
+  </div>
   </div>
 </template>
 <script setup lang="ts">
--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-4">
+  <div class="mb-4 relative">
     <h2 class="text-3xl text-darkColor font-semibold mb-2">All Users</h2>
     <div>
       <input
@@ -7,6 +7,7 @@
         class="bg-transparent border-2 px-2 border-darkColor py-0.5 rounded-lg"
       />
     </div>
+    <SharedDeleteUserModal :modal-visible="deleteModal" @closeModal="deleteModal = !deleteModal"/>
   </div>
   <div class="bg-darkColor rounded-md w-full min-h-[300px]">
     <ul
@@ -22,27 +23,49 @@
       <li class="px-3 w-1/5">Actions</li>
     </ul>
     <ul
-      v-for="item , i in 10" :key="i"
-      class="w-full py-2 px-2 gap-x-2 border-b border-gray flex items-center text-white text-base hover:bg-gray  duration-150"
+      v-for="(item, i) in 10"
+      :key="i"
+      class="w-full py-2 px-2 gap-x-2 border-b border-gray flex items-center text-white text-base hover:bg-gray duration-150"
     >
       <li class="px-2">
         <input type="checkbox" name="" id="" />
       </li>
-      <li class="px-3 w-1/5"><img src="@/assets/images/user-image.png" alt="user image" class="w-[50px] rounded-full"></li>
+      <li class="px-3 w-1/5">
+        <img
+          src="@/assets/images/user-image.png"
+          alt="user image"
+          class="w-[50px] rounded-full"
+        />
+      </li>
       <li class="px-3 w-1/5">Samandar</li>
       <li class="px-3 w-1/5">300</li>
       <li class="px-3 w-1/5">Uzbekistan</li>
       <li class="px-3 w-1/5 flex items-center gap-x-2">
         <!-- <button class="px-4 py-0.5 bg-blue rounded-md font-medium">Edit</button> -->
-        <button class="px-4 py-0.5 bg-red rounded-md">Delete</button>
+        <button
+          class="px-4 py-0.5 bg-red rounded-md"
+          @click="deleteModal = !deleteModal"
+        >
+          Delete
+        </button>
       </li>
     </ul>
   </div>
 </template>
 <script setup lang="ts">
+const deleteModal = ref(false);
 definePageMeta({
   layout: "admin",
 });
 
-const heading = ["#", "Image", "Username", "Country", "Date"];
+onMounted(() => {
+  window.addEventListener("click", (e:any) => {
+    const el = e.target.getAttribute('data-name')
+
+    if(el === 'overlay') {
+      deleteModal.value = false
+    }
+  })
+})
+// const heading = ["#", "Image", "Username", "Country", "Date"];
 </script>
