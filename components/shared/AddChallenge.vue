756d4b033e57dd746702290c60c5bfbe741149d3
Samandar <samandarwebdeveloper@gmail.com>
2024-05-20 17:44:37 +0500

    fix

--- components/shared/AddChallenge.vue
+++ components/shared/AddChallenge.vue
@@ -5,7 +5,7 @@
     :class="`${modalVisible ? 'visible duration-75' : 'invisible'}`"
   ></div>
   <div
-    class="fixed top-0 w-[250px] lg:w-1/5 h-full z-40 cursor-pointer bg-darkColor rounded-tl-md py-3 shadow-md px-2"
+    class="fixed top-0 w-[250px] lg:w-1/5 h-full overflow-y-auto z-40 cursor-pointer bg-darkColor rounded-tl-md py-3 shadow-md px-2"
     :class="`${
       modalVisible
         ? 'visible right-0 duration-150'
@@ -20,7 +20,7 @@
           type="text"
           placeholder="Title"
           class="text-sm w-full px-2 py-1 bg-white border border-white rounded-md placeholder:text-gray text-blakc outline-none"
-          :class="{'border-red' : errInfo.title}"
+          :class="{ 'border-red': errInfo.title }"
         />
       </label>
       <label class="text-white inline-block mb-2">
@@ -31,7 +31,7 @@
           rows="5"
           placeholder="Description"
           class="text-sm w-full border border-white bg-white min-h-[100px] max-h-[100px] px-2 py-1 rounded-md placeholder:text-gray text-blakc outline-none"
-          :class="{'border-red': errInfo.desc}"
+          :class="{ 'border-red': errInfo.desc }"
         ></textarea>
       </label>
 
@@ -40,17 +40,30 @@
         <input
           placeholder="Response"
           class="text-sm w-full px-2 py-1 bg-white border border-white rounded-md placeholder:text-gray text-blakc outline-none"
-          :class="{'border-red' : errInfo.resp}"
+          :class="{ 'border-red': errInfo.resp }"
         />
       </label>
       <label class="text-white inline-block mb-2 w-full relative">
         <span class="text-lg font-medium inline-block mb-2">Type</span>
-        <h2 @click="selectVisible = !selectVisible" class="text-sm w-full bg-white px-2 py-1 rounded-md placeholder:text-gray text-blakc outline-none"
-        >{{ type }}</h2>
-        <ul v-if="selectVisible"  class="absolute top-[-100%] w-full bg-white text-blakc rounded-md">
-            <li @click="clickSelect('Easy')" class="px-2 py-1 hover:bg-gray">Easy</li>
-            <li @click="clickSelect('Medium')" class="px-2 py-1 hover:bg-gray">Medium</li>
-            <li @click="clickSelect('Hard')" class="px-2 py-1 hover:bg-gray">Hard</li>
+        <h2
+          @click="selectVisible = !selectVisible"
+          class="text-sm w-full bg-white px-2 py-1 rounded-md placeholder:text-gray text-blakc outline-none"
+        >
+          {{ type }}
+        </h2>
+        <ul
+          v-if="selectVisible"
+          class="absolute top-[-100%] w-full bg-white text-blakc rounded-md"
+        >
+          <li @click="clickSelect('Easy')" class="px-2 py-1 hover:bg-gray">
+            Easy
+          </li>
+          <li @click="clickSelect('Medium')" class="px-2 py-1 hover:bg-gray">
+            Medium
+          </li>
+          <li @click="clickSelect('Hard')" class="px-2 py-1 hover:bg-gray">
+            Hard
+          </li>
         </ul>
       </label>
       <label class="text-white inline-block mb-6">
@@ -59,27 +72,29 @@
           type="file"
           placeholder="title"
           class="text-sm w-full px-2 py-1 border border-white rounded-md bg-white placeholder:text-gray text-blakc outline-none"
-          :class="{'border-red' : errInfo.file}"
+          :class="{ 'border-red': errInfo.file }"
         />
       </label>
 
       <div class="grid grid-cols-2 items-center justify-center gap-x-2">
-        <button type="submit" class="bg-blue px-2 py-1 text-white text-lg font-medium rounded-lg">Add</button>
-        <button @click="$emit('closeModal')" class="bg-white px-2 py-1 text-darkColor text-lg font-medium rounded-lg">Cansel</button>
+        <button
+          type="submit"
+          class="bg-blue px-2 py-1 text-white text-lg font-medium rounded-lg"
+        >
+          Add
+        </button>
+        <button
+          @click="$emit('closeModal')"
+          class="bg-white px-2 py-1 text-darkColor text-lg font-medium rounded-lg"
+        >
+          Cansel
+        </button>
       </div>
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
-const selectVisible = ref(false)
-const type = ref('Easy')
-const errInfo = ref({
-    title: false,
-    desc:false,
-    resp:false,
-    file: false
-})
 const props = defineProps({
   modalVisible: {
     type: Boolean,
@@ -87,12 +102,19 @@ const props = defineProps({
   },
 });
 
-const clickSelect = (e:string) => {
-    selectVisible.value = false
-    type.value = e
-}  
+const selectVisible = ref(false);
+const type = ref("Easy");
+const errInfo = ref({
+  title: false,
+  desc: false,
+  resp: false,
+  file: false,
+});
 
-const handleClick = () => {
+const clickSelect = (e: string) => {
+  selectVisible.value = false;
+  type.value = e;
+};
 
-}
+const handleClick = () => {};
 </script>
